3f11090092a836d9aa528e6739a85a89ae5d8d68
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-04-02 23:48:19 +0530

    create footer & navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Navbar = () => {
+    const [sticky, setSticky]=useState(false)
+    useEffect(() => {
+        const handleScroll=()=>{
+            if(window.scrollY > 0){
+                setSticky(true)
+            }
+            else{
+                setSticky(false)
+            }
+        }
+        window.addEventListener('scroll',handleScroll)
+        return () => {
+            window.removeEventListener('scroll',handleScroll)
+        }
+        
+    },[])
   const navItems = (
     <>
       <li>
@@ -19,8 +35,16 @@ const Navbar = () => {
   );
   return (
     <>
-      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
-        <div className="navbar bg-base-100">
+         <div
+        className={` max-w-screen-2xl container mx-auto md:px-20 px-4 dark:bg-slate-800 dark:text-white fixed top-0 left-0 right-0 z-50 ${
+          sticky
+            ? "sticky-navbar shadow-md bg-base-200 dark:bg-slate-700 dark:text-white duration-300 transition-all ease-in-out"
+            : ""
+        }`}
+            >
+                
+                
+        <div className="navbar ">
           <div className="navbar-start">
             <div className="dropdown">
               <div
@@ -50,7 +74,7 @@ const Navbar = () => {
                 {navItems}
               </ul>
             </div>
-            <a className=" text-2xl font-bold cursor-pointer">bookStore</a>
+            <a className=" text-2xl font-bold cursor-pointer">BOOKSTORE</a>
           </div>
           <div className="navbar-end space-x-3">
             <div className="navbar-center hidden lg:flex">
