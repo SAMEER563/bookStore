54aa9e47af3a4ce4832b4e54fa4d5544a4198988
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-04-03 11:16:13 +0530

    Dark & light mode

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div>
         
         <hr className='mt-5' />
-      <footer className="footer footer-center p-10  text-base-content rounded">
+      <footer className="footer footer-center p-10  text-base-content rounded dark:bg-slate-900 dark:text-white">
   <nav className="grid grid-flow-col gap-4">
     <a className="link link-hover">About us</a>
     <a className="link link-hover">Contact</a>
