27cc4c102ecd8dab4bf610e4f8eeff26c9854c50
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-04-06 22:32:46 +0530

    about

---
+++ frontend/src/abouts/Abouts.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import About from '../components/About'
+import Footer from '../components/Footer'
+
+function Abouts() {
+  return (
+    <div>
+          <>
+    <Navbar/>
+<div className='min-h-screen'>
+<About />
+</div>
+<Footer />
+</>
+    </div>
+  )
+}
+
+export default Abouts
