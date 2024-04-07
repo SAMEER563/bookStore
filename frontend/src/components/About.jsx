f3ea16cf97299d2fe4ef05ec8783f669130b5f8c
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-04-07 23:57:24 +0530

    change

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -6,7 +6,7 @@ function About() {
    <div className='max-w-screen-2x1 container mx-auto md:px-20 px-4 flex flex-col md:flex-row my-16'>
         <div className='w-full md:w-1/2'>
             <h1 className='items-center '>About Us</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam cum veniam dolore praesentium, inventore ea libero minima at, repellat odio sed quis quo tempore maxime ex obcaecati voluptatibus. Veritatis neque a voluptatibus eveniet cumque rerum qui aliquid reprehenderit voluptatum aperiam!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam cum veniam dolore praesentium, inventore ea libero minima at, repellat odio sed quis quo tempore maxime ex obcaecati voluptatibus. <span className='text-pink-500 '>Veritatis neque a voluptatibus eveniet cumque rerum qui aliquid reprehenderit voluptatum aperiam!</span></p>
         </div>
     <div className='w-full md:w-1/2'>
         <img src="https://img.freepik.com/free-photo/front-view-stacked-books-ladders-education-day_23-2149241046.jpg?w=996" className='h-70 rounded-xl mt-5 ml-14 w-92 h-60' alt=''/>
