dbae75cf47c5318b233f8d02f1ac9cfd5179e4d0
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-04-07 23:33:44 +0530

    about page change

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -4,18 +4,13 @@ function About() {
   return (
    <>
    <div className='max-w-screen-2x1 container mx-auto md:px-20 px-4 flex flex-col md:flex-row my-16'>
-    <div className='w-full md:w-1/2'>
-        <img src="https://source.unsplash.com/1600x900/?about" className='h-70 rounded-xl mt-5 ml-14 w-92 h-60' alt=''/>
-        <div className='mt-8 flex items-center '>
-        <span className="loading loading-spinner text-primary"></span>
-<span className="loading loading-spinner text-secondary"></span>
-<span className="loading loading-spinner text-accent"></span>
-<span className="loading loading-spinner text-neutral"></span>
-<span className="loading loading-spinner text-info"></span>
-<span className="loading loading-spinner text-success"></span>
-<span className="loading loading-spinner text-warning"></span>
-<span className="loading loading-spinner text-error"></span>
+        <div className='w-full md:w-1/2'>
+            <h1 className='items-center '>About Us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam cum veniam dolore praesentium, inventore ea libero minima at, repellat odio sed quis quo tempore maxime ex obcaecati voluptatibus. Veritatis neque a voluptatibus eveniet cumque rerum qui aliquid reprehenderit voluptatum aperiam!</p>
         </div>
+    <div className='w-full md:w-1/2'>
+        <img src="https://img.freepik.com/free-photo/front-view-stacked-books-ladders-education-day_23-2149241046.jpg?w=996" className='h-70 rounded-xl mt-5 ml-14 w-92 h-60' alt=''/>
+        
         </div>
    </div>
    </>
