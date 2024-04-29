f009d1f9ae06136a2e4c364368f142b52d5ba2f0
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-04-29 22:03:36 +0530

    pre deploy change

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,17 @@ import cors from 'cors';
 
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
+import path from 'path';
+
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 dotenv.config();
+
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 const URI = process.env.MongoDBURI;
@@ -30,6 +35,16 @@ try {
 // defining routes
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
+
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+}
+);
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
